cb6766834b4db8b592a033d373c2fd671122cbaa
Nixon Puertollano <nixoncpuertollano@gmail.com>
2024-10-21 23:23:01 -0400

    finish event handler states

--- src/app/commissions/CommissionsForm.jsx
+++ src/app/commissions/CommissionsForm.jsx
@@ -4,21 +4,36 @@ const CommissionsForm = (props) => {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
+  const [commissionType, setCommissionType] = useState('')
+  const [pieceVision, setPieceVision] = useState('')
+  const [symmetryType, setSymmetryType] = useState('')
+  const [baseMaterial, setBaseMaterial] = useState('')
   const [colors, setColors] = useState('')
   const [fabrics, setFabrics] = useState('')
+  const [shapePatterns, setShapePatterns] = useState('')
+  const [distress, setDistress] = useState('')
+  const [retailor, setRetailor] = useState('')
   const [pockets, setPockets] = useState('')
+  const [weeklyChecks, setWeeklyChecks] = useState('')
+  const [extra, setExtra] = useState('')
 
-  const [commissionType, setCommissionType] = useState('')
-  const [symmetryType, setSymmetryType] = useState('')
 
   const handleFirstName = (e) => setFirstName(e.target.value)
   const handleLastName = (e) => setLastName(e.target.value)
   const handleEmail = (e) => setLastName(e.target.value)
   const handleCommissionType = (e) => setCommissionType(e.target.value)
+  const handlePieceVision = (e) => setPieceVision(e.pieceVision)
   const handleSymmetryType = (e) => setSymmetryType(e.target.value)
+  const handleBaseMaterial = (e) => setBaseMaterial(e.baseMaterial.value)
   const handleColors = (e) => setColors(e.target.value)
   const handleFabrics = (e) => setFabrics(e.target.value)
+  const handleShapePatterns = (e) => setShapePatterns(e.target.value)
+  const handleDistress = (e) => setDistress(e.target.value)
+  const handleRetailor = (e) => setRetailor(e.target.value)
   const handlePockets = (e) => setPockets(e.target.value)
+  const handleWeeklyChecks = (e) => setWeeklyChecks(e.target.value)
+  const handleExtra = (e) => setExtra(e.target.value)
+
 
   return (
     <form className='flex flex-col'>
@@ -48,7 +63,7 @@ const CommissionsForm = (props) => {
       </label>
       <label className="flex">
         Describe the vision you have for the piece in as much detail as possible.
-        <textarea name="pieceVision" placeholder="Type your message" className="border-solid border-2 border-sky-500" />
+        <textarea name="pieceVision" placeholder="Type your message" className="border-solid border-2 border-sky-500" onChange={handlePieceVision}/>
       </label>
       <label>
         Do you like symmetry, asymmetry, or a mix?
@@ -77,11 +92,11 @@ const CommissionsForm = (props) => {
         You receive a 10% discount for providing the base clothes.
         <label>
           Yes, I'm providing the base
-          <input type="radio" name='baseMaterial' value="Yes, I'm providing the base" />
+          <input type="radio" name='baseMaterial' value="Yes, I'm providing the base" onChange={handleBaseMaterial} />
         </label>
         <label>
           No, I want you to source all the materials.
-          <input type="radio" name='baseMaterial' value="No, I want you to source all the materials." />
+          <input type="radio" name='baseMaterial' value="No, I want you to source all the materials." onChange={handleBaseMaterial} />
         </label>
       </label>
       <label>
@@ -94,28 +109,28 @@ const CommissionsForm = (props) => {
       </label>
       <label className="flex">
         Please provide some loose detail on shape patterns you prefer
-        <textarea name='shapePatterns' placeholder="Type your message" className="border-solid border-2 border-sky-500" />
+        <textarea name='shapePatterns' placeholder="Type your message" className="border-solid border-2 border-sky-500" onchange={handleShapePatterns} />
       </label>
       <label>
         Would you like to add some distress (wear and tear)?
         <label>
           Yes
-          <input type="radio" name='distress' value="Yes" />
+          <input type="radio" name='distress' value="Yes" onChange={handleDistress}/>
         </label>
         <label>
           No
-          <input type="radio" name='distress' value="No" />
+          <input type="radio" name='distress' value="No" onChange={handleDistress} />
         </label>
       </label>
       <label>
         If it's an old piece, do you need it retailored?
         <label>
           Yes
-          <input type="radio" name='retailor' value="Yes" />
+          <input type="radio" name='retailor' value="Yes" onChange={handleRetailor}/>
         </label>
         <label>
           No
-          <input type="radio" name='retailor' value="No" />
+          <input type="radio" name='retailor' value="No" onChange={handleRetailor} />
         </label>
       </label>
       <label>
@@ -126,16 +141,16 @@ const CommissionsForm = (props) => {
         Do you want me to send you weekly check-ins?
         <label>
           Yes
-          <input type="radio" name='weeklyChecks' value="Yes" />
+          <input type="radio" name='weeklyChecks' value="Yes" onChange={handleWeeklyChecks}/>
         </label>
         <label>
           No
-          <input type="radio" name='weeklyChecks' value="No" />
+          <input type="radio" name='weeklyChecks' value="No" onChange={handleWeeklyChecks}/>
         </label>
       </label>
       <label className="flex">
         Anything else I should know?
-        <textarea name='extra ' placeholder="Type your message" className="border-solid border-2 border-sky-500" />
+        <textarea name='extra ' placeholder="Type your message" className="border-solid border-2 border-sky-500" onChange={handleExtra}/>
       </label>
       <button>Submit</button>
     </form>
