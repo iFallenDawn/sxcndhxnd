296ef1aa3eeb00356a4807d7ad3c79d98deb2960
Nikky Soriano <57335776+nikkyjsoriano@users.noreply.github.com>
2025-06-06 21:32:32 -0400

    Package and Component Cleanup (#20)
    - Remove more unused components and packages

--- tempo-starter/src/components/navbar.tsx
+++ tempo-starter/src/components/navbar.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { createClient } from "../../supabase/server";
-import { Button } from "./ui/button";
+import { Button } from "@chakra-ui/react";
 import { User, UserCircle } from "lucide-react";
 import UserProfile from "./user-profile";
 
